ab7cca11accbaa588bf353e9302c6f111b7f90c0
Tiago de Paula <tiagodepalves@gmail.com>
2023-05-16 20:43:02 -0300

    matsum: Checked integer multiplication

--- 01-MatSum/src/matsum-parallel.cu
+++ 01-MatSum/src/matsum-parallel.cu
@@ -1,14 +1,33 @@
 #include <cmath>
 #include <omp.h>
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <optional>
 #include <stdexcept>
 #include <tuple>
 
 /** Marker for conditional expressions that are unlikely to happen. */
 #define unlikely(condition) (__builtin_expect(!!(condition), false))
+template <typename Num>
+/** Does 'a * b' if possible, or returns 'nullopt' if an overflow or underflow would occur. */
+static constexpr inline std::optional<Num> checked_mul(const Num a, const Num b) noexcept {
+  if (a == 0) {
+    return a * b;
+  }
+
+  constexpr Num MINUMUM = std::numeric_limits<Num>::min();
+  constexpr Num MAXIMUM = std::numeric_limits<Num>::max();
+  const Num lo = std::min(MINUMUM / a, MAXIMUM / a);
+  const Num hi = std::max(MINUMUM / a, MAXIMUM / a);
+
+  if unlikely (b < lo || b > hi) {
+    return std::nullopt;
+  }
+  return a * b;
+}
 
 /** Parse matrix dimensions from test case. */
 static std::pair<unsigned, unsigned> read_input(const char *filename) {
@@ -18,8 +37,7 @@ static std::pair<unsigned, unsigned> read_input(const char *filename) {
   unsigned rows, cols;
   input >> rows >> cols;
 
-  constexpr unsigned max_size = std::numeric_limits<unsigned>::max();
-  if unlikely (rows > max_size / cols) {
+  if unlikely (!checked_mul(rows, cols).has_value()) {
     throw std::length_error("matrix has more elements than UINT_MAX");
   }
   return std::pair(rows, cols);
